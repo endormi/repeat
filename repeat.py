c3d5d0d0f3fe50187efab573377d0be0ae419ae6
endormi <39559256+endormi@users.noreply.github.com>
2023-06-06 21:07:45 +0300

    Quick updates

--- repeat.py
+++ repeat.py
@@ -1,16 +1,15 @@
-import argparse, sys
+import argparse
+import sys
+from charred import repeat_char
 
-def repeat_char(char, num_of_times):
-    return (char * (num_of_times//len(char) + 1))[:num_of_times]
 
 def main():
     parser = argparse.ArgumentParser(description='Repeat a single character number of times.')
-    required = parser.add_argument_group('required arguments')
-    required.add_argument("-c", "--c", metavar='C', type=str, required=True,
-                        default=None, help='character to repeat')
-    parser.add_argument("-n", "--n", metavar='N', type=int, default=6,
+    parser.add_argument("-c", metavar='C', type=str, required=True,
+                        help='character to repeat')
+    parser.add_argument("-n", metavar='N', type=int, default=6,
                         help='repeat number of times')
-    parser.add_argument("-o", "--o", metavar='out', type=str, default=False,
+    parser.add_argument("-o", metavar='out', type=str, default=False,
                         help='PATH to output file')
     args = parser.parse_args()
 
@@ -18,11 +17,14 @@ def main():
         print("Too many characters, only use 1.")
         sys.exit()
 
+    output = repeat_char(args.c, args.n)
+
     if args.o:
         with open(args.o, "w") as f:
-            f.write(repeat_char(args.c, args.n))
+            f.write(output)
     else:
-        print(repeat_char(args.c, args.n))
+        print(output)
+
 
 if __name__ == '__main__':
     main()
